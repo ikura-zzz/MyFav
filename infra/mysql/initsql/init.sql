04fa69e8880c2dcbcf129b0dd9569ab363f8b2f4
ikura-zzz <chukicamata_kbmc@yahoo.co.jp>
2021-07-08 18:35:01 +0900

    go ginのタグ埋め込み

--- infra/mysql/initsql/init.sql
+++ infra/mysql/initsql/init.sql
@@ -1,17 +1,12 @@
-SET @old_autocommit=@@autocommit;
-
+SET @old_autocommit = @@autocommit;
 CREATE DATABASE `myfav` DEFAULT CHARACTER SET utf8mb4;
-
 USE `myfav`;
-
 DROP TABLE IF EXISTS `appusers`;
-
 CREATE TABLE `appusers` (
     `id` int NOT NULL AUTO_INCREMENT,
     `username` char(50) NOT NULL DEFAULT '',
     `passhash` binary(32) NOT NULL DEFAULT '',
     `upddate` datetime NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-SET autocommit=@old_autocommit;
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+SET autocommit = @old_autocommit;
