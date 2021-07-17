96797f4f110a715b95c5b9365fd037b31f295b39
ikura-zzz <chukicamata_kbmc@yahoo.co.jp>
2021-07-17 09:41:46 +0900

    fav登録用テーブル作成

--- infra/mysql/initsql/init.sql
+++ infra/mysql/initsql/init.sql
@@ -3,12 +3,38 @@ CREATE DATABASE `myfav` DEFAULT CHARACTER SET utf8mb4;
 USE `myfav`;
 DROP TABLE IF EXISTS `appusers`;
 CREATE TABLE `appusers` (
-    `id` int NOT NULL AUTO_INCREMENT,
+    `userid` int NOT NULL AUTO_INCREMENT,
     `username` char(50) NOT NULL UNIQUE,
     `passhash` binary(32) NOT NULL DEFAULT '',
     `upddate` datetime NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`userid`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+DROP TABLE IF EXISTS `favs`;
+CREATE TABLE `favs` (
+    `favid` int NOT NULL AUTO_INCREMENT,
+    `userid` int NOT NULL,
+    `title` char(100) NOT NULL,
+    `category` char(50) DEFAULT '',
+    `publisher` char(50) DEFAULT '',
+    `overview` TEXT,
+    `impression` MEDIUMTEXT,
+    `timing` char(1) NOT NULL,
+    `stars` char(1) NOT NULL,
+    `openclose` char(1) NOT NULL,
+    `upddate` datetime NOT NULL,
+    PRIMARY KEY (`favid`),
+    FOREIGN KEY (userid) REFERENCES appusers(userid)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE `images`(
+    `imageid` int NOT NULL AUTO_INCREMENT,
+    `favid` int NOT NULL,
+    `image` MEDIUMTEXT NOT NULL,
+    PRIMARY KEY (`imageid`),
+    FOREIGN KEY (favid) REFERENCES favs(favid)
+)ENGINE = InnoDB;
 
 SET autocommit = @old_autocommit;
 
