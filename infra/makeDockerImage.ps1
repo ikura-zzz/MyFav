10cd58bcf691e75a20318689b73bcb407624a602
ikura-zzz <chukicamata_kbmc@yahoo.co.jp>
2021-07-23 00:47:02 +0900

    ビルド時にmysqlを除外するように変更

--- infra/makeDockerImage.ps1
+++ infra/makeDockerImage.ps1
@@ -2,15 +2,25 @@ Set-Location .\goserv\
 .\gomake.ps1
 Set-Location ..\
 
-docker rm -f $(docker ps -q -a)
+#docker rm -f $(docker ps -q -a)
+$nginx=((docker ps -a | Select-String mynginx).Line).Split(" ")[0]
+$goserv=((docker ps -a | Select-String goserv).Line).Split(" ")[0]
+$mysql=((docker ps -a | Select-String mysql).Line).Split(" ")[0]
+docker rm -f $nginx
+docker rm -f $goserv
+#docker rm -f $mysql
+
 docker rmi $(docker images goserv -q)
 docker rmi $(docker images mynginx -q)
-docker rmi $(docker images mysql -q)
+#docker rmi $(docker images mysql -q)
 
 docker build -t mynginx nginx/
 docker build -t goserv goserv/
-docker build -t mysql mysql/
+#docker build -t mysql mysql/
 
 docker run --privileged --network mysql-network -p 8080:8080 -d -it --name goserv goserv /sbin/init
 docker run --network mysql-network -p 80:80 -d -it --name mynginx mynginx
-docker run --network mysql-network -p 3306:3306 -d -it --name mysql mysql
+#docker run --network mysql-network -p 3306:3306 -d -it --name mysql mysql
+If ( $mysql = "\r\n" ) {
+    docker start mysql
+}
--- infra/makeDockerImage.sh
+++ infra/makeDockerImage.sh
@@ -1,4 +1,11 @@
-docker rm -f $(docker ps -q -a)
+#docker rm -f $(docker ps -q -a)
+mynginx=`docker ps | grep mynginx | awk '{print $1}'`
+goserv=`docker ps | grep goserv | awk '{print $1}'`
+mysql=`docker ps | grep mysql | awk '{print $1}'`
+docker rm -f $mynginx
+docker rm -f $goserv
+#docker rm -f $mysql
+
 docker rmi $(docker images goserv -q)
 docker rmi $(docker images mynginx -q)
 #docker rmi $(docker images mysql -q)
@@ -11,4 +18,7 @@ docker build -t goserv goserv/
 
 docker run --privileged --network mysql-network -p 8080:8080 -d -it --name goserv goserv /sbin/init
 docker run --network mysql-network -p 80:80 -d -it --name mynginx mynginx
-docker run --network mysql-network -p 3306:3306 -d -it --name mysql mysql
+#ocker run --network mysql-network -p 3306:3306 -d -it --name mysql mysql
+if [[ $mysql = "" ]]; then
+    docker start mysql
+fi
