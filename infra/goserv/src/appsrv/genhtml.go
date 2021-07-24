42cbffb00b8d7f91e676652a1edbfd405c39a438
ikura-zzz <chukicamata_kbmc@yahoo.co.jp>
2021-07-24 16:23:38 +0900

    変更前コミット

--- infra/goserv/src/appsrv/genhtml.go
+++ infra/goserv/src/appsrv/genhtml.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"myfav/favmanager"
 	"myfav/sessionmanager"
 	"myfav/utils"
@@ -12,42 +13,22 @@ import (
 
 const checked string = "checked"
 
-func genlistall(c *gin.Context) (string, error) {
-	return genlist(c, utils.SelectFavsByUserid)
+func genlist(c *gin.Context) (string, error) {
+	return genlistbody(c, utils.SelectFavsByUserid)
 }
 
-func genlistalready(c *gin.Context) (string, error) {
-	return genlist(c, utils.SelectFavsByUseridAlready)
-}
-func genlistnow(c *gin.Context) (string, error) {
-	return genlist(c, utils.SelectFavsByUseridNow)
+func genlist_foreign(c *gin.Context) (string, error) {
+	return genlistbody(c, utils.SelectFavsByUserid)
 }
 
-func genlistwish(c *gin.Context) (string, error) {
-	return genlist(c, utils.SelectFavsByUseridWish)
-}
-func genlistall_foreign(c *gin.Context) (string, error) {
-	return genlist(c, utils.SelectFavsByUserid)
-}
-
-func genlistalready_foreign(c *gin.Context) (string, error) {
-	return genlist(c, utils.SelectFavsByUseridAlready)
-}
-func genlistnow_foreign(c *gin.Context) (string, error) {
-	return genlist(c, utils.SelectFavsByUseridNow)
-}
-
-func genlistwish_foreign(c *gin.Context) (string, error) {
-	return genlist(c, utils.SelectFavsByUseridWish)
-}
-func genlist(c *gin.Context, sql string) (string, error) {
+func genlistbody(c *gin.Context, sql string) (string, error) {
 	userid, err := sessionmanager.GetUserId(c)
 	if err != nil {
 		return "", errors.New("selectfav_getuserid " + err.Error())
 	}
 	favs, err := favmanager.Selectfavs(userid, sql)
 	if err != nil {
-		return "", errors.New("genlistall_selectfavs:" + err.Error())
+		return "", errors.New("genlist_selectfavs:" + err.Error())
 	}
 	return genhtml(favs)
 }
@@ -60,13 +41,20 @@ func genhtml(favs []favmanager.Fav) (string, error) {
 
 		fav.Stars = starsconv(fav.Stars)
 
-		html += "<a href=\"" + link +
-			"\">	<li class=\"inline-block border-b border-gray-300 flex justify-between items-center py-4\">		<div class=\"flex items-start w-2/5\">			<div class=\"w-10 h-10 rounded mr-3\">				<img src=\"" +
-			fav.Icon + "\" class=\"rounded-full h-10 w-10 bg-gray-300 m-auto\">			</div>			<div class=\"flex-1 overflow-hidden\">				<div>					<span class=\"font-bold \">" +
-			fav.Title + "</span>				</div>				<p class=\"text-gray-700 text-xs\">" +
-			fav.Category + "</p>			</div>		</div>		<p class=\"w-2/5\">" +
-			fav.Overview + "</p>		<label for=\"stars\" class=\"font-bold w-1/5 text-right\">" +
-			fav.Stars + "</label>	</li></a>\n"
+		html += "<a class=\"fav\" id=\"fav" + fmt.Sprintf("%d", i) + "\" href=\"" + link + "\" style=\"display: none;\">" +
+			"<input type=\"hidden\" id=\"timing" + fmt.Sprintf("%d", i) + "\" value=\"" + fav.Timing + "\" style=\"display: none;\">" +
+			"<li class=\"inline-block border-b border-gray-300 flex justify-between items-center py-4\" style=\"display: none;\">" +
+			"<div class=\"flex items-start w-2/5\">" +
+			"<div class=\"w-10 h-10 rounded mr-3\">" +
+			"<img src=\"" + fav.Icon + "\" class=\"rounded-full h-10 w-10 bg-gray-300 m-auto\">" +
+			"</div>" +
+			"<div class=\"flex-1 overflow-hidden\">" +
+			"<span class=\"font-bold \">" + fav.Title + "</span>" +
+			"<p class=\"text-gray-700 text-xs\">" + fav.Category + "</p>" +
+			"</div>" +
+			"</div>" +
+			"<p class=\"w-2/5\">" + fav.Overview + "</p>" +
+			"<label for=\"stars\" class=\"font-bold w-1/5 text-right\">" + fav.Stars + "</label></li></a>\n"
 	}
 	return html, nil
 }
@@ -96,9 +84,9 @@ func gentimingHTML(t string) string {
 		wish = checked
 	}
 	html := "<input type=\"radio\" id=\"already\" name=\"timing\" value=\"already\" " +
-		already + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"already\">前から</label><input type=\"radio\" id=\"now\" name=\"timing\" value=\"now\" " +
-		now + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"now\">いま好き</label><input type=\"radio\" id=\"wish\" name=\"timing\" value=\"wish\" " +
-		wish + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"wish\">興味ある</label>"
+		already + "><label class=\"tab_item text-base tracking-wide px-3\" for=\"already\">前から</label><input type=\"radio\" id=\"now\" name=\"timing\" value=\"now\" " +
+		now + "><label class=\"tab_item text-base tracking-wide px-3\" for=\"now\">いま好き</label><input type=\"radio\" id=\"wish\" name=\"timing\" value=\"wish\" " +
+		wish + "><label class=\"tab_item text-base tracking-wide px-3\" for=\"wish\">興味ある</label>"
 	return html
 }
 
@@ -121,11 +109,11 @@ func genstarsHTML(s string) string {
 		five = checked
 	}
 	html := "<input type=\"radio\" id=\"one\" name=\"stars\" value=\"1\" " +
-		one + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"one\">1</label><input type=\"radio\" id=\"two\" name=\"stars\" value=\"2\" " +
-		two + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"two\">2</label><input type=\"radio\" id=\"three\" name=\"stars\" value=\"3\" " +
-		three + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"three\">3</label><input type=\"radio\" id=\"four\" name=\"stars\" value=\"4\" " +
-		four + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"four\">4</label><input type=\"radio\" id=\"five\" name=\"stars\" value=\"5\" " +
-		five + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"five\">5</label>"
+		one + "><label class=\"tab_item text-base tracking-wide px-5\" for=\"one\">1</label><input type=\"radio\" id=\"two\" name=\"stars\" value=\"2\" " +
+		two + "><label class=\"tab_item text-base tracking-wide px-5\" for=\"two\">2</label><input type=\"radio\" id=\"three\" name=\"stars\" value=\"3\" " +
+		three + "><label class=\"tab_item text-base tracking-wide px-5\" for=\"three\">3</label><input type=\"radio\" id=\"four\" name=\"stars\" value=\"4\" " +
+		four + "><label class=\"tab_item text-base tracking-wide px-5\" for=\"four\">4</label><input type=\"radio\" id=\"five\" name=\"stars\" value=\"5\" " +
+		five + "><label class=\"tab_item text-base tracking-wide px-5\" for=\"five\">5</label>"
 	return html
 }
 func genopencloseHTML(oc string) string {
@@ -138,7 +126,7 @@ func genopencloseHTML(oc string) string {
 		close = checked
 	}
 	html := "<input type=\"radio\" id=\"open\" name=\"openclose\" value=\"open\" " +
-		open + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"open\">公開</label><input type=\"radio\" id=\"close\" name=\"openclose\" value=\"close\" " +
-		close + "><label class=\"tab_item text-base tracking-wide px-6\" for=\"close\">非公開</label>"
+		open + "><label class=\"tab_item text-base tracking-wide px-10\" for=\"open\">公開</label><input type=\"radio\" id=\"close\" name=\"openclose\" value=\"close\" " +
+		close + "><label class=\"tab_item text-base tracking-wide px-10\" for=\"close\">非公開</label>"
 	return html
 }
