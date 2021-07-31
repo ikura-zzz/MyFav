9a892d0d9cd0e58228e42e24b543fdefd03b001a
ikura-zzz <chukicamata_kbmc@yahoo.co.jp>
2021-08-01 00:00:50 +0900

    Windows用ログパス修正

--- infra/makeDockerImage.ps1
+++ infra/makeDockerImage.ps1
@@ -18,7 +18,7 @@ docker build -t mynginx nginx/
 docker build -t goserv goserv/
 #docker build -t mysql mysql/
 
-docker run --privileged --network mysql-network -v "/var/log/myfav:/var/log/myfav" -p 8080:8080 -d -it --name goserv goserv /sbin/init
+docker run --privileged --network mysql-network -v "/c/var/log/myfav:/var/log/myfav" -p 8080:8080 -d -it --name goserv goserv /sbin/init
 docker run --network mysql-network -p 80:80 -d -it --name mynginx mynginx
 #docker run --network mysql-network -p 3306:3306 -d -it --name mysql mysql
 If ( $mysql = "\r\n" ) {
