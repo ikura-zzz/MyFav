c4d5860000d8eb49606426a425b169ca99ce754d
ikura-zzz <chukicamata_kbmc@yahoo.co.jp>
2021-08-10 20:23:25 +0900

    証明書更新スクリプト

--- infra/updatePem.sh
+++ infra/updatePem.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo docker run -it --rm --name certbot -p 80:80 -p 443:443 -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" certbot/certbot certonly
+shlog=/var/log/myfav/sh.log
+/home/docker/workspace/myfav/infra/stopContainers.sh >> $shlog
+sudo docker run -it --rm --name certbot -p 80:80 -p 443:443 -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" certbot/certbot renew >> $shlog
+sudo docker run -it --rm --name certbot -p 80:80 -p 443:443 -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" certbot/certbot certificates >> $shlog
+/home/docker/workspace/myfav/infra/startContainers.sh >> $shlog
